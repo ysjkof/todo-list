08d3ed6868ab821a1cfade13203749f992e50b28
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-08-07 19:25:47 +0900

    feat: TanStack Query 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import Router from './router/Router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './styles/tailwind.css';
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <Router />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
