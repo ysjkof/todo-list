8961f3b1e024b10e29af111ef3025c90c763259f
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-08-08 11:22:53 +0900

    feat: removeItemInArrayByIndex 함수 제네릭 적용

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,8 @@
 export const cls = (...rest: any[]) => rest.join(' ');
 
-export const removeItemInArrayByIndex = (index: number, array: any[]) => [
-  ...array.slice(0, index),
-  ...array.slice(index + 1),
-];
+export function removeItemInArrayByIndex<T>(index: number, array: T[]) {
+  return [...array.slice(0, index), ...array.slice(index + 1)];
+}
 
 export function changeValueInArray<T>(array: T[], value: T, index: number) {
   return [...array.slice(0, index), value, ...array.slice(index + 1)];
