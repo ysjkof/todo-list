b3f3b874a67ab4ee515a5aea65db58d3b920442f
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-08-10 09:06:11 +0900

    feat: InputWithLabel, TextareaWithLabel 타입 단언(as) 제거
    - ref에 타입단언 제거

--- src/components/molecules/InputWithLabel.tsx
+++ src/components/molecules/InputWithLabel.tsx
@@ -5,19 +5,13 @@ interface InputWithLabelProps
   label: string;
 }
 
-export type RefTypeWithNull =
-  | React.RefObject<HTMLInputElement>
-  | null
-  | undefined;
-
-export default forwardRef(function InputWithLabel(
-  { label, ...rest }: InputWithLabelProps,
-  ref
-) {
-  return (
-    <label className="flex flex-col w-full gap-2 text-gray-700 text-sm">
-      {label}
-      <input className="border-b px-4" {...rest} ref={ref as RefTypeWithNull} />
-    </label>
-  );
-});
+export default forwardRef<HTMLInputElement, InputWithLabelProps>(
+  function InputWithLabel({ label, ...rest }, ref) {
+    return (
+      <label className="flex flex-col w-full gap-2 text-gray-700 text-sm">
+        {label}
+        <input className="border-b px-4" {...rest} ref={ref} />
+      </label>
+    );
+  }
+);
--- src/components/molecules/TextareaWithLabel.tsx
+++ src/components/molecules/TextareaWithLabel.tsx
@@ -1,27 +1,17 @@
 import { forwardRef } from 'react';
 
 interface TextareaWithLabelProps
-  extends React.InputHTMLAttributes<HTMLTextAreaElement> {
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
   label: string;
 }
 
-export type RefTypeWithNull =
-  | React.RefObject<HTMLTextAreaElement>
-  | null
-  | undefined;
-
-export default forwardRef(function TextareaWithLabel(
-  { label, ...rest }: TextareaWithLabelProps,
-  ref
-) {
-  return (
-    <label className="flex flex-col w-full gap-2 text-gray-700 text-sm">
-      {label}
-      <textarea
-        className="border px-4"
-        {...rest}
-        ref={ref as RefTypeWithNull}
-      />
-    </label>
-  );
-});
+export default forwardRef<HTMLTextAreaElement, TextareaWithLabelProps>(
+  function TextareaWithLabel({ label, ...rest }, ref) {
+    return (
+      <label className="flex w-full flex-col gap-2 text-sm text-gray-700">
+        {label}
+        <textarea className="border px-4" {...rest} ref={ref} />
+      </label>
+    );
+  }
+);
