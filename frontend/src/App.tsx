756d89b1cb846c9886e036edeacdf3ecd79648fb
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-08-18 18:12:56 +0900

    style: 안쓰는 import 삭제

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useState } from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import Router from './router/Router';
 import { getUserToken } from './services/authServices';
